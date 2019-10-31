75c043cf971dc47368bf4dc7e53c561dd90192df
Brendan Flood <brendan_flood@yahoo.com>
2019-10-31 09:21:18 -0500

    final attempt at this playground

--- DatesPlayground.playground/Contents.swift
+++ DatesPlayground.playground/Contents.swift
@@ -7,7 +7,7 @@ let date = Date()
 // Create a date using today's date and a DateComponents structure
 let thisDate = date
 let calendar = Calendar.current
-//let thisDateComponents = calendar.dateComponents([Calendar.Component.day, Calendar.Component.month, Calendar.Component.year], from: thisDate)
+
 var thatDateComponents = DateComponents()
 thatDateComponents.day = 31
 thatDateComponents.month = 10
@@ -15,19 +15,17 @@ thatDateComponents.year = 2019
 let thatDate = calendar.date(from: thatDateComponents)
 
 // Compare the two dates to see if they occur in the same Calendar day
-var sameDate = true
-//if(thisDateComponents.day == thatDateComponents.day && thisDateComponents.month == thatDateComponents.month && thisDateComponents.year == thatDateComponents.year){
-    sameDate = true
-//}else{
-//    sameDate = false
-//}
+var sameDate = calendar.isDate(thisDate, equalTo: thatDate!, toGranularity: .day)
 // Use a date formatter
 
 let formatter = DateFormatter()
 formatter.dateFormat = "EEEE, MMMM d, y"
-let dateString = formatter.string(from: date)
+let dateString = formatter.string(from: thisDate)
+let otherDateString = formatter.string(from: thatDate!)
 // use the formatter to print the day in a friendly format.
 // hint, use NSDateFormatter.com to get an appropriate printing format string.
 
 
 print(dateString)
+print(otherDateString)
+print(sameDate)
